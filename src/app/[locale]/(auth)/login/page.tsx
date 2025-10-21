0cb7203a0014455ae17b2085f4c02f3783d7af64
zaphodh <luispaulo.ni@gmail.com>
2025-10-20 22:04:40 -0300

    added redirect to dashboard

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { getCurrentSession } from "@/lib/server/auth/session";
 
 export default async function Login() {
     const { session } = await getCurrentSession();
-    if (session) return redirect("/lobby");
+    if (session) return redirect("/dashboard");
     return (
         <section>
             <div className="container mx-auto">
