a41e08654ebcf0f0ccdb32a07b640308e10ba16b
zaphodh <luispaulo.ni@gmail.com>
2025-11-11 09:42:48 -0300

    added listing to the left and image bg

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -4,7 +4,7 @@ import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useScopedI18n } from '@/locales/client'
-
+import Car from "@/../public/car.jpg"
 export default function HeroSection() {
     const t = useScopedI18n('marketing.hero')
 
@@ -14,27 +14,17 @@ export default function HeroSection() {
                 <div className="relative mx-auto max-w-5xl px-6 py-28 lg:py-20">
                     <div className="lg:flex lg:items-center lg:gap-12">
                         <div className="relative z-10 mx-auto max-w-xl text-center lg:ml-0 lg:w-1/2 lg:text-left">
-                            <Link
-                                href="/"
-                                className="rounded-(--radius) mx-auto flex w-fit items-center gap-2 border p-1 pr-3 lg:ml-0">
-                                <span className="bg-muted rounded-[calc(var(--radius)-0.25rem)] px-2 py-1 text-xs">New</span>
-                                <span className="text-sm">{t('powering')}</span>
-                                <span className="bg-(--color-border) block h-4 w-px"></span>
-
-                                <ArrowRight className="size-4" />
-                            </Link>
-
                             <h1 className="mt-10 text-balance text-4xl font-bold md:text-5xl xl:text-5xl">{t('title')}</h1>
                             <p className="mt-8">{t('subtitle')}</p>
 
                             <div>
                                 <div className="my-10 lg:my-12">
                                     <Button asChild size="lg" variant="outline" className="w-full sm:w-auto">
-                                        <Link href="#pricing">{t('viewPricing')}</Link>
+                                        <Link href="/pricing">{t('viewPricing')}</Link>
                                     </Button>
                                 </div>
 
-                                <ul className="list-outside list-disc space-y-2 text-sm pl-5">
+                                <ul className="list-outside list-disc space-y-2 text-sm pl-5 text-left">
                                     <li>{t('faster')}</li>
                                     <li>{t('modern')}</li>
                                     <li>{t('customizable')}</li>
@@ -47,14 +37,14 @@ export default function HeroSection() {
                             <div className="bg-radial-[at_65%_25%] to-background z-1 -inset-17 absolute from-transparent to-40%"></div>
                             <Image
                                 className="hidden dark:block"
-                                src="/music.png"
+                                src={Car.src}
                                 alt="app illustration"
                                 width={2796}
                                 height={2008}
                             />
                             <Image
                                 className="dark:hidden"
-                                src="/music-light.png"
+                                src={Car.src}
                                 alt="app illustration"
                                 width={2796}
                                 height={2008}
