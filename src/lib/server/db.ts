33cba44660b364fc774e8e2761e035b5f6bbe296
zaphodh <luispaulo.ni@gmail.com>
2025-12-05 09:39:16 -0300

    tryna figure out the error of prisma migration

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,14 +1,20 @@
 
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const adapter = new PrismaPg({
+const pool = new Pool({
     connectionString: process.env.DB_PRISMA_URL,
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000,
 })
+
+const adapter = new PrismaPg(pool)
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
     adapter,
